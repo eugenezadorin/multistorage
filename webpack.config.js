5a823574aa1bf36ae6d7ab21b71db7838c063a7b
Eugene Zadorin <mail@eugenezadorin.ru>
2017-10-06 01:05:28 +0300

    added minimal webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+var path = require('path')
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'multistorage.min.js',
+        library: 'MultiStorage',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['add-module-exports']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ]
+}
